9cf74190d233cc9e6786465b6f78f81b4cdc506d
Janiss Binder <binder@puzzle.ch>
2018-06-18 17:44:44 +0200

    insert missing alumni into correct alumnus_group

--- db/migrate/20180613124429_create_missing_alumnus_members.rb
+++ db/migrate/20180613124429_create_missing_alumnus_members.rb
@@ -5,46 +5,58 @@ class CreateMissingAlumnusMembers < ActiveRecord::Migration
 
   def up
     missing_alumni = select_rows(find_missing_people_sql)
+    return if missing_alumni.blank?
+    alumni_groups = select_rows(find_alumni_groups_sql(missing_alumni.collect(&:second).uniq))
 
-    return unless missing_alumni
-    
-    execute(insert_roles_sql(missing_alumni))
+    execute(insert_roles_sql(missing_alumni, alumni_groups.index_by(&:first)))
   end
 
   private
 
   def find_missing_people_sql
     <<-SQL
-    SELECT DISTINCT person_id, layer_group_id, roles.type FROM roles 
+    SELECT DISTINCT person_id, layer_group_id FROM roles 
     INNER JOIN groups ON roles.group_id = groups.id
     WHERE roles.type IN (#{sanitize(role_types)})
     AND roles.deleted_at IS NOT NULL
     AND person_id NOT IN (
       SELECT DISTINCT person_id FROM roles
       INNER JOIN groups g ON roles.group_id = g.id
-      WHERE g.layer_group_id = layer_group_id
+      WHERE g.layer_group_id = groups.layer_group_id
       AND roles.deleted_at IS NULL
       AND (
         roles.type IN (#{sanitize(role_types)}) OR
         roles.type IN ('Group::FlockAlumnusGroup::Member', 'Group::ChildGroup::Child'))
     )
+    AND layer_group_id NOT IN (
+      SELECT DISTINCT id from groups
+      WHERE deleted_at IS NOT NULL
+    )
     GROUP BY person_id, layer_group_id
     SQL
   end
+
+  def find_alumni_groups_sql(layer_group_ids)
+    <<-SQL
+    SELECT layer_group_id, id FROM groups
+    WHERE layer_group_id IN (#{sanitize(layer_group_ids)})
+    AND groups.type = 'Group::FlockAlumnusGroup'
+    SQL
+  end
   
-  def insert_roles_sql(list)
+  def insert_roles_sql(list, alumni_groups)
     now = Role.sanitize(Time.zone.now)
     <<-SQL
-    INSERT INTO roles(created_at, updated_at, person_id, group_id, label, type)
-    VALUES #{values(list, now).join(',')}
+    INSERT INTO roles(created_at, updated_at, person_id, group_id, type)
+    VALUES #{values(list, alumni_groups, now).join(',')}
     SQL
   end
 
-  def values(list, now)
-    list.collect do |person_id, group_id, role_type|
+  def values(list, alumnus_group_memo, now)
+    list.collect do |person_id, layer_group_id|
+      _, group_id = alumnus_group_memo.fetch(layer_group_id)
       "(#{now}, #{now}, #{person_id}, #{group_id},"\
-        " #{role_type.constantize.model_name.human},"\
-        " Group::FlockAlumnusGroup::Member)"
+        " 'Group::FlockAlumnusGroup::Member')"
     end
   end
 
